8d9717b3a58ecff943b590876bc89fb86e3d3786
Skyler Robbins <ssssssssssssss>
2024-12-14 12:11:11 -0700

    Completed Part 2

--- Contents.swift
+++ Contents.swift
@@ -2,18 +2,108 @@ import Cocoa
 
 // MARK: Part 1
 
-var urlComponents = URLComponents(string: "https://itunes.apple.com/search")!
-urlComponents.queryItems = [
-    "term": "Beyonders",
-    "media": "audiobook",
-    "attribute": "titleTerm",
-    "lang": "en_us"
-].map { URLQueryItem(name: $0.key, value: $0.value) }
+//
+//var urlComponents = URLComponents(string: "https://itunes.apple.com/search")!
+//urlComponents.queryItems = [
+//    "term": "Apple",
+//    "media": "ebook",
+//    "attribute": "authorTerm",
+//    "lang": "en_us",
+//    "limit": "10"
+//].map { URLQueryItem(name: $0.key, value: $0.value) }
+//
+//Task {
+//    let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
+//
+//    if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+//        data.prettyPrintedJSONString()
+//    }
+//}
+
+// MARK: Part 2
+
+struct StoreItem: Codable {
+    let name: String
+    let artist: String
+    var kind: String
+    var description: String
+    var artworkURL: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "trackName"
+        case artist = "artistName"
+        case kind
+        case description = "longDescription"
+        case artworkURL = "artworkUrl100"
+    }
+    
+    enum AdditionalKeys: String, CodingKey {
+        case longDescription
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: CodingKeys.name)
+        artist = try values.decode(String.self, forKey: CodingKeys.artist)
+        kind = try values.decode(String.self, forKey: CodingKeys.kind)
+        artworkURL = try values.decode(URL.self, forKey: CodingKeys.artworkURL)
+
+        if let description = try? values.decode(String.self, forKey: CodingKeys.description) {
+            self.description = description
+        } else {
+            let additionalValues = try decoder.container(keyedBy: AdditionalKeys.self)
+            description = (try? additionalValues.decode(String.self, forKey: AdditionalKeys.longDescription)) ?? ""
+        }
+    }
+}
+
+struct SearchResponse: Codable {
+    let results: [StoreItem]
+}
+
+enum StoreItemError: Error, LocalizedError {
+    case itemsNotFound
+}
+
+func fetchItems(matching query: [String: String]) async throws -> [StoreItem] {
+    var urlComponents = URLComponents(string: "https://itunes.apple.com/search")!
+    urlComponents.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+    
+    let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
+    
+    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+        throw StoreItemError.itemsNotFound
+    }
+    
+    let decoder = JSONDecoder()
+    let searchResponse = try decoder.decode(SearchResponse.self, from: data)
+
+    return searchResponse.results
+}
+
+let query = [
+    "term": "Apple",
+    "media": "ebook",
+    "attribute": "authorTerm",
+    "lang": "en_us",
+    "limit": "10"
+]
 
 Task {
-    let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
+    do {
+        let storeItems = try await fetchItems(matching: query)
+        storeItems.forEach { item in
+            print("""
+            Name: \(item.name)
+            Artist: \(item.artist)
+            Kind: \(item.kind)
+            Description: \(item.description)
+            Artwork URL: \(item.artworkURL)
 
-    if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200, let string = String(data: data, encoding: .utf8) {
-        print(string)
+
+            """)
+        }
+    } catch {
+        print(error)
     }
 }
